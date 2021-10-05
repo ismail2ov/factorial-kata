f42ca0030aa2b3d57de6dfe8ee46e2f29031ffbf
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-10-05 23:36:41 +0200

    Add tests

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,8 +1,8 @@
 public class Factorial {
-    public int of(int i) {
+    public long of(long i) {
         if (i < 2 ) {
             return 1;
         }
-        return 2;
+        return this.of(i-1) * i;
     }
 }
--- src/test/java/FactorialShould.java
+++ src/test/java/FactorialShould.java
@@ -1,33 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialShould {
 
+    private Factorial factorial;
+
+    @BeforeEach
+    void setUp() {
+        factorial = new Factorial();
+    }
+
     @Test
     void return_1_for_factorial_of_0() {
-        Factorial factorial = new Factorial();
-
-        int actual = factorial.of(0);
+        long actual = factorial.of(0);
 
         assertEquals(1, actual);
     }
 
     @Test
     void return_1_for_factorial_of_1() {
-        Factorial factorial = new Factorial();
-
-        int actual = factorial.of(1);
+        long actual = factorial.of(1);
 
         assertEquals(1, actual);
     }
 
     @Test
     void return_2_for_factorial_of_2() {
-        Factorial factorial = new Factorial();
-
-        int actual = factorial.of(2);
+        long actual = factorial.of(2);
 
         assertEquals(2, actual);
+    }
+
+    @Test
+    void return_6_for_factorial_of_3() {
+        long actual = factorial.of(3);
+
+        assertEquals(6, actual);
     }
 }
